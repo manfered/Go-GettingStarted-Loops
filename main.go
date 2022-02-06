cc77b8d8b11ced0d253ef16090fdf762f03f68dc
manfered <farzad.seifi@gmail.com>
2022-02-06 16:15:15 -0700

    loop through collection, clises and map data value

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ func main() {
 	loopTillConditionWithPostClause(3)
 
 	infiniteLoop()
+
+	loopOverCollection()
 }
 
 // while equivalent loop
@@ -32,5 +34,31 @@ func infiniteLoop() {
 		}
 		println(index)
 		index++
+	}
+}
+
+// for each loop equivalent
+func loopOverCollection() {
+	testSlice := []int{1, 2, 3, 4}
+
+	// grabbing the index and value
+	for index, value := range testSlice {
+		println(index, value)
+	}
+
+	// grabbing index only
+	for index := range testSlice {
+		println(index)
+	}
+
+	// grabbing value only by using WriteOnly variable for index
+	for _, value := range testSlice {
+		println(value)
+	}
+
+	// loop through map data type
+	testMap := map[string]int{"Fred": 1, "Manfered": 2}
+	for key, value := range testMap {
+		println(key, value)
 	}
 }
